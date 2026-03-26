21df0564905465075143c15ec40bf0f2c0005dd8
ANDY-KINGS <andykng63@gmail.com>
2026-03-26 21:53:03 +0300

    Update CORS to allow Vercel previews

--- backend/config/security.js
+++ backend/config/security.js
@@ -5,7 +5,13 @@ import rateLimit from 'express-rate-limit';
 export default (app) => {
     app.use(helmet());
     app.use(cors({
-        origin: process.env.CLIENT_URL || '*',
+        origin: function (origin, callback) {
+            if (!origin) return callback(null, true);
+            const allowedOrigin = process.env.CLIENT_URL;
+            if (allowedOrigin && origin === allowedOrigin) return callback(null, true);
+            if (origin.endsWith('.vercel.app') || origin.startsWith('http://localhost:')) return callback(null, true);
+            return callback(new Error('Not allowed by CORS (' + origin + ')'));
+        },
         credentials: true
     }));
     app.use(rateLimit({
